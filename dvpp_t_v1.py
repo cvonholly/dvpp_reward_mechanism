ab0e9ed4012d0eb63d1f467aeab1c75581d80ec6
Carl von Holly <cvonholly@ikmail.com>
2025-09-20 09:51:32 +0200

    new plots with battery model change

--- dvpp_t_v1.py
+++ dvpp_t_v1.py
@@ -4,7 +4,7 @@ DVPP consisting of Solar PV, Wind and Battery
 """
 
 # import procduction data
-from src.get_device_systems import get_pv_sys, get_wind_sys, get_bess_io_sys
+from src.get_device_systems import get_pv_sys, get_wind_sys, get_bess_io_sys, get_bess_energy_sys
 
 from run_dvpp_simulation import run_dvpp_simulation
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     def get_io_dict():
         return {'PV': (get_pv_sys(), 'lpf', 1),
                 'Wind': (get_wind_sys(), 'lpf', 1),
-                'BESS': (get_bess_io_sys(t_drop=8), 'hpf', 1),
+                'BESS': (get_bess_energy_sys(e_max=1.8), 'hpf', 1),
                 }
                 
     run_dvpp_simulation(get_io_dict,
--- dvpp_t_v3.py
+++ dvpp_t_v3.py
@@ -3,7 +3,7 @@ DVPP consisting of Hydro, BESS and Supercapacitor
 """
 
 # import procduction data
-from src.get_device_systems import get_hydro_tf, get_bess_io_sys, get_sc_io_sys
+from src.get_device_systems import get_hydro_tf, get_bess_io_sys, get_sc_io_sys, get_bess_energy_sys
 
 from run_dvpp_simulation import run_dvpp_simulation
 
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # run simulation with default parameters
     def get_io_dict():
         return {'Hydro': (get_hydro_tf(), 'lpf', 1),
-                'BESS': (get_bess_io_sys(t_drop=8), 'bpf', 1),
+                'BESS': (get_bess_energy_sys(e_max=1.8), 'hpf', 1),
                 'SC': (get_sc_io_sys(t_drop=5), 'hpf', 1),
                 }
                 
