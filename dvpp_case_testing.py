1897aad3315261ef83f6ae9f13ee4a0f7e51c45c
Carl von Holly <44812683+cvonholly@users.noreply.github.com>
2026-01-08 08:36:38 +0100

    new testing script

---
+++ dvpp_case_testing.py
@@ -0,0 +1,75 @@
+"""
+DVPP consisting of Solar PV, Wind and BESS 
+    with case study and uncertainty in Pv and Wind generation
+
+Ilmar Hybdrid DVPP:
+wind_cap = 216  # MW
+solar_cap = 150  # MW
+battery_cap, battery_energy = 25, 50  # MW, MWh
+- use 10% of the capacity for the services
+
+run scenarios with uncertainties
+    1. stage: optimize grand coalition expected reward
+    2. stage: real-time operation based on actual generation, compute value function
+
+todo:
+- BESS energy is limited -> make dependend on previous energy level
+
+assumptions:
+    - pv and wind generation are varying based on scenarios, forecast based on historical 2-month data for the hour
+    - we bid the capacity based on the maximum possible reward in the forecasted generation
+    - the rating is set as follows:
+        if set_service_rating:
+            sum_service_rating = set_service_rating[subset]
+        else:
+            sum_service_rating = sum([v[2] for v in subset_io_dict.values() if v[1]=='lpf']) if service!='FFR' else sum([v[2] for v in subset_io_dict.values()]) 
+"""
+
+import pandas as pd
+
+from src.get_required_services import get_fcr_d, get_ffr_fcr, get_ffr_fcr_d
+from run_case_dvpp_sim import run_case_dvpp_sim
+from src.get_device_systems import get_pv_sys, get_wind_sys, get_bess_io_sys, get_bess_energy_sys
+
+
+if __name__ == '__main__':
+    # run simulation with default parameters
+    rel = .1  # 10 percent of the capacity
+    # wind_cap = 216 * rel  # MW
+    # solar_cap = 150 * rel  # MW
+    # battery_cap, battery_energy = 25 * rel, 50 * rel * 3600 # MW, MWh
+    wind_cap = 5
+    solar_cap = 5
+    battery_cap, battery_energy = 2.5, 5 # MW, MWh
+    def get_io_dict():
+        return {'PV': (get_pv_sys(), 'lpf', solar_cap),
+                'Wind': (get_wind_sys(), 'lpf', wind_cap),
+                'BESS': (get_bess_energy_sys(e_max=battery_energy), 'hpf', battery_cap),
+                }
+
+    # normal scenario period: 2025-04-06 10:00:00
+    # max wind error: 2024-12-19 15:00:00
+    # normal period: 2025-04-06 00:00:00 to 2025-04-12 23:00:00
+    start_date, end_date = pd.to_datetime(['2025-04-06 12:00:00', '2025-04-06 14:00:00'])
+    
+    # allow sub-coalitions to form
+    allow_sub_coalitions = True
+    # def save_pics(i):
+    #     return i < 3  # save pics for first 3 scenarios only
+
+    run_case_dvpp_sim(get_io_dict,
+                        save_path='pics/v_TESTING',
+                        services_input={'FFR + FCR-D': get_ffr_fcr_d()},
+                        STATIC_PF=False,
+                        K_errors=3,  # change to 20!!! # number of scenarios for the uncertainty
+                        save_pics=True,
+                        time_slots=(start_date, end_date),
+                        set_special_ratings={
+                            'FFR + FCR-D': {
+                                ('BESS',): 1,  # 100 percent of the DC gain
+                            }
+                        },
+                        save_dvpp_info=True,
+                        hourly_average=True,
+                        allow_sub_coalitions=allow_sub_coalitions
+                        )
